c47ccbce490f72a8976b587a35d95432dd3f70b2
lunginspector <138542747+lunginspector@users.noreply.github.com>
2026-03-07 09:25:36 -0800

    Initial Commit

---
+++ Lithium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lithium
+//
+//  Created by lunginspector on 3/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lithium/LithiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LithiumApp.swift
+//  Lithium
+//
+//  Created by lunginspector on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LithiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
