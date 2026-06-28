c3436b5c3c133cfbe2932548de28f338a5d894a8
lunginspector <138542747+lunginspector@users.noreply.github.com>
2026-06-28 11:11:21 -0700

    Initial Commit

---
+++ Lithium/LithiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LithiumApp.swift
+//  Lithium
+//
+//  Created by lunginspector on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LithiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
